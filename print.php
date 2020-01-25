99bb16381778d0015231c47d215b0caed61c657f
jayapriya11 <60217311+jayapriya11@users.noreply.github.com>
2020-01-25 21:08:44 +0530

    Creating new file

---
+++ print.php
@@ -0,0 +1,16 @@
+<?php
+for($i=1;$i<=100;$i++){
+  switch ($i){
+    case ($i%3 == 0 && $i%5 == 0):
+      echo "Linianos<br/>";
+      break;
+    case $i%3 == 0:
+      echo "Linio<br/>";
+      break;
+    case $i%5 == 0:
+      echo "IT<br/>";
+      break;
+  }  
+}
+
+?>
